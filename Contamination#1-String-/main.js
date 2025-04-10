e80282d9747cdb21c7fcfd8fb0d2f45df058d8fe
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-04-10 15:48:08 +0200

    first 2 exercises of sprint 1

---
+++ Contamination#1-String-/main.js
@@ -0,0 +1,27 @@
+/*
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+*/
+
+function contamination() {
+    if(originalText === null || originalText === undefined || originalText.length === 0 || character === null || character === undefined || character.length === 0 || character.length > 1) {
+        originalText = '';
+    } else {
+        let letters = originalText.split("");
+        originalText = '';
+        console.log(letters);
+        for (let i = 0; i < letters.length; i++) {
+            originalText += character;
+        }
+        console.log(letters);
+    }
+    return originalText;
+}
